89780d16e99141f8eb496594c38352a76eeb5d53
Henrique Cardoso <henrique.jc2016@gmail.com>
2023-08-03 02:02:36 -0300

    importei meu arquivo modo-escuro.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initScrollsuave from './modules/scroll-suave.js';
 import initReturnTop from './modules/return-top.js';
+import initModoEscuro from './modules/modo-escuro.js';
 
 initScrollsuave();
 initReturnTop();
+initModoEscuro();

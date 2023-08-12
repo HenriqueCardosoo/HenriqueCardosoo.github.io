bc2429f43187cdffff2ac585223ffa2df01ab80f
Henrique Cardoso <henrique.jc2016@gmail.com>
2023-08-11 21:50:38 -0300

    script para redirecionar para o projeto

---
+++ js/modules/div-links.js
@@ -0,0 +1,6 @@
+export default function initDivLinks() {
+  const linkProjetos = document.querySelector('.rs6');
+  linkProjetos.addEventListener('click', () => {
+    window.location.href = 'https://henriquecardosoo.github.io/Audi.github.io/';
+  });
+}

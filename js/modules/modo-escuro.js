4fc582d0e006d1f2733c3cedee93f27978de2d7c
Henrique Cardoso <henrique.jc2016@gmail.com>
2023-08-13 00:59:43 -0300

    add localStorage modo escuro

--- js/modules/modo-escuro.js
+++ js/modules/modo-escuro.js
@@ -6,4 +6,14 @@ const body = document.body;
 modoEscuro.addEventListener('click', () => {
   body.classList.toggle('modo-dark');
   console.log('modoEscuro');
+
+  const darkMode = body.classList.contains('modo-dark');
+  localStorage.setItem('modoEscuro', darkMode.toString());
+});
+
+window.addEventListener('load', () => {
+  const salvarModo = localStorage.getItem('modoEscuro');
+  if (salvarModo === 'true') {
+    body.classList.add('modo-dark');
+  }
 });

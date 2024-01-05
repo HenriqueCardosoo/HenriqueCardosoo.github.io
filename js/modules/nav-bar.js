dde8d7897c5c1a6d9dae7066fd06be26c8dbf676
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-01-05 13:13:37 -0300

    desativei scrool quando o menu está aberto

--- js/modules/nav-bar.js
+++ js/modules/nav-bar.js
@@ -1,9 +1,11 @@
 export default function navBar() {
   const hamburger = document.querySelector('.hamburger');
   const nav = document.querySelector('.nav');
+  const bodyMenu = document.querySelector('body');
 
   hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     nav.classList.toggle('active');
+    bodyMenu.classList.toggle('menuOpen');
   });
 }

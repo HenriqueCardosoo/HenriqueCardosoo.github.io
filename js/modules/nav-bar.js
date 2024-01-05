c556572bd1a6ee00ffd3ddf8bab1446266750c2e
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-01-05 13:18:59 -0300

    update body

--- js/modules/nav-bar.js
+++ js/modules/nav-bar.js
@@ -1,11 +1,11 @@
 export default function navBar() {
   const hamburger = document.querySelector('.hamburger');
   const nav = document.querySelector('.nav');
-  const bodyMenu = document.querySelector('body');
+  const body = document.body;
 
   hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     nav.classList.toggle('active');
-    bodyMenu.classList.toggle('menuOpen');
+    body.classList.toggle('menuOpen');
   });
 }

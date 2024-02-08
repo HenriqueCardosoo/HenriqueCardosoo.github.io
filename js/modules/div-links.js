c86fb24fb783a5f819762403671dcdbe87c81b00
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-02-08 00:58:35 -0300

    add evento click nos container dos projetos

--- js/modules/div-links.js
+++ js/modules/div-links.js
@@ -3,4 +3,14 @@ export default function initDivLinks() {
   linkProjetos.addEventListener('click', () => {
     window.location.href = 'https://henriquecardosoo.github.io/Audi.github.io/';
   });
+
+  const iPhone = document.querySelector('.iphone');
+  iPhone.addEventListener('click', () => {
+    window.location.href = 'https://henriquecardosoo.github.io/apple.github.io/';
+  });
+
+  const timer = document.querySelector('.timer');
+  timer.addEventListener('click', () => {
+    window.location.href = 'https://henriquecardosoo.github.io/timer.github.io/';
+  });
 }
